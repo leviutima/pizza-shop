67898ee7ea5ce38c19150ab474dda9bf39483162
levi utima <levi.utima@gmail.com>
2024-12-19 18:53:12 -0300

    feat: first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { RouterProvider } from 'react-router-dom'
+import './index.css'
+import { router } from './routes'
+
+export function App() {
+
+  return (
+    <RouterProvider router={router}/>
+  )
+}
