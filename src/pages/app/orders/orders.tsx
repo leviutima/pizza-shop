54e7e23fea6c118b670fc802e042a0f3845e8339
levi utima <levi.utima@gmail.com>
2024-12-27 18:54:53 -0300

    feat: implement pagination and funcionality buttons

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,52 +1,83 @@
-import { Table, TableBody, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import { Helmet } from "react-helmet-async";
 import { OrderTableRow } from "./order-table-row";
 import { OrderTableFilter } from "./order-table-filters";
 import { Pagination } from "@/components/pagination";
 import { useQuery } from "@tanstack/react-query";
 import { getOrders } from "@/api/get-orders";
+import { useSearchParams } from "react-router-dom";
+import { z } from "zod";
 
 export function Orders() {
+  const [searchParams, setSearchParams] = useSearchParams();
 
-    const { data: result } = useQuery({
-        queryKey: ["orders"],
-        queryFn: getOrders,
+  const pageIndex = z.coerce
+    .number()
+    .transform((page) => page - 1)
+    .parse(searchParams.get("page") ?? "1");
+
+  const { data: result } = useQuery({
+    queryKey: ["orders", pageIndex],
+    queryFn: () =>
+      getOrders({
+        pageIndex,
+      }),
+  });
+
+  function handlePaginate(pageIndex: number) {
+    setSearchParams(url => {
+      url.set('page',( pageIndex + 1).toString())
+      
+      return url
     })
+  }
 
-    return(
-        <div>
-            <Helmet title="Pedidos"/>
-            <div className="flex flex-col gap-4">
-                <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
-            
-            <div className="space-y-2.5">
-                <OrderTableFilter />
-                <div className="border rounded-md">
-                    <Table>
-                        <TableHeader>
-                            <TableRow>
-                                <TableHead className="w-[64px]"></TableHead>
-                                <TableHead className="w-[140px]">Identificador</TableHead>
-                                <TableHead className="w-[180px]">Realizado há</TableHead>
-                                <TableHead className="w-[140px]">Status</TableHead>
-                                <TableHead className="w-[40vw]">Cliente</TableHead>
-                                <TableHead className="w-[140px]">Total de pedido</TableHead>
-                                <TableHead className="w-[164px]"></TableHead>
-                                <TableHead className="w-[132px]"></TableHead>
-                            </TableRow>
-                        </TableHeader>
-                        <TableBody>
-                            {result && result.orders.map((item) => {
-                                return(
-                                    <OrderTableRow key={item.orderId} order={item}/>
-                                )
-                            }) }
-                        </TableBody>
-                    </Table>
-                    </div>
-                    <Pagination pageIndex={0} totalCount={105} perPage={10}/>
-                </div>
-            </div>
+  return (
+    <div>
+      <Helmet title="Pedidos" />
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
+
+        <div className="space-y-2.5">
+          <OrderTableFilter />
+          <div className="rounded-md border">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[64px]"></TableHead>
+                  <TableHead className="w-[140px]">Identificador</TableHead>
+                  <TableHead className="w-[180px]">Realizado há</TableHead>
+                  <TableHead className="w-[140px]">Status</TableHead>
+                  <TableHead className="w-[40vw]">Cliente</TableHead>
+                  <TableHead className="w-[140px]">Total de pedido</TableHead>
+                  <TableHead className="w-[164px]"></TableHead>
+                  <TableHead className="w-[132px]"></TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {result &&
+                  result.orders.map((item) => {
+                    return <OrderTableRow key={item.orderId} order={item} />;
+                  })}
+              </TableBody>
+            </Table>
+          </div>
+          {result && (
+            <Pagination
+              onPageChange={handlePaginate}
+              pageIndex={result.meta.pageIndex}
+              totalCount={result.meta.totalCount}
+              perPage={result.meta.perPage}
+            />
+          )}
         </div>
-    )
+      </div>
+    </div>
+  );
 }
